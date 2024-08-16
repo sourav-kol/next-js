6611f35c558448174de1655a84ab7bd84515f190
sourav15-99 <sourav.kolambkar@teknorix.com>
2024-08-16 19:12:13 +0530

    changed the project structure

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { RootLayout } from './layout'
+//import '../public/styles.scss';
+
+
+export default function Home() {
+  return (
+    <RootLayout>
+      <h1>Home Page</h1>
+    </RootLayout>
+    
+  );
+}
---
+++ pages/layout.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Layout, Flex } from 'antd';
+// import { Header, Footer, Content } from 'antd/es/layout/layout';
+// import Sider from 'antd/es/layout/Sider';
+
+const { Header, Sider, Footer, Content } = Layout;
+
+const metadata = {
+  title: 'Story',
+  description: 'Tell your story',
+}
+
+type Props = {
+  children: React.ReactNode
+}
+
+export function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <Layout className='layout'>
+      <Header className='header'>
+        <div className="logo"><p>Logo</p></div>
+      </Header>
+      <Content className='content'>
+        {children}
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Next Js Footer
+      </Footer>
+    </Layout>
+  )
+}
